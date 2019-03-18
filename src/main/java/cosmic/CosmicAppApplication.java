d25d56cbbc4c525612424908a8a59c00a4b6e291
Damian Grochowski <smg7@o2.pl>
2019-03-18 09:53:02 +0100

    init

---
+++ src/main/java/cosmic/CosmicAppApplication.java
@@ -0,0 +1,13 @@
+package cosmic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmicAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosmicAppApplication.class, args);
+	}
+
+}
